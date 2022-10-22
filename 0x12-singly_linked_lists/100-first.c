67aefeddcf2ecbe04dace684f3c96b6fe72f5dc5
Vanbliser <ayogublossomisrael@gmail.com>
2022-10-22 09:24:27 +0000

    Fix betty error in 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,7 +9,7 @@ void startUpFunc(void) __attribute__ ((constructor));
  */
 void startUpFunc(void)
 {
-	printf("You're beat! and yet, you must allow,\n\
-I bore my house upon my back!\n");
+	printf(
+"You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
 
