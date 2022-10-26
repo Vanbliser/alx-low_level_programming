72ddf78c7759ba172a24f335d21b8b50b5ab7430
Vanbliser <ayogublossomisrael@gmail.com>
2022-10-26 01:30:09 +0000

    Fix betty error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * insert_nodeint_at_index - a function that inserts a new node at a given
- * position. 
+ * position.
  * Description: where idx is the index of the list where the new node should
  * be added. Index starts at 0. if it is not possible to add the new node
  * at index idx, do not add the new node and return NULL
@@ -44,6 +44,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			ptr_head = ptr_head->next;
 	}
 
-	return (NULL);	
+	return (NULL);
 }
 
