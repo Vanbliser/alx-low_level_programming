43193e97cb1ea68b640b1245d42241ffc8b19229
Vanbliser <ayogublossomisrael@gmail.com>
2022-10-28 18:45:19 +0000

    Add 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * get_bit - a function that returns the value of a bit at a given index.
+ * @n: a decimal digit
+ * @index: the index
+ * Description: where index is the index, starting from 0 of the bit you
+ * want to get
+ *
+ * Return: the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int __attribute__ ((unused)) div, mod, counter = 0;
+
+	while (n)
+	{
+		div = n / 2;
+		mod = n % 2;
+		if (counter == index)
+			return ((int)mod);
+		counter++;
+		n = div;
+	}
+	return (-1);
+}
+
