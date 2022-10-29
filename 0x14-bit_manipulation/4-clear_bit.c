40eb1857d3426c795d59e56a7176efb1d1d14958
Vanbliser <ayogublossomisrael@gmail.com>
2022-10-29 01:43:06 +0000

    Update4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,11 +10,17 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int a = 1 << index;
-	*n = *n & (~a);
-	if (*n == 0)
-		return (-1);
+	unsigned long int a;
+	
+	if (index < 32)
+	{
+		a = 1 << index;
+		*n = *n & (~a);
+		return (1);
+	}
 	else
-		return (0);
+	{
+		return (-1);
+	}
 }
 
