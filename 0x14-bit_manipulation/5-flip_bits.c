5f50e37f393acd4c16d78f62256cb01677a071bc
Vanbliser <ayogublossomisrael@gmail.com>
2022-10-29 02:11:18 +0000

    Add 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,32 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long __attribute__ ((unused)) i, old_start, new_start, sum;
+	unsigned long int a = n ^ m;
+	unsigned int count = 0;
 
-
+	if (a == 0)
+	{
+		return (count);
+	}
+	else
+	{
+		for (i = 1; i <= n; i *= 2)
+			new_start = i;
+		count++;
+		sum = new_start;
+		while (new_start > 1)
+		{
+			old_start = new_start;
+			for (i = 1; i < old_start; i *= 2)
+				new_start = i;
+			if ((sum + new_start) <= a)
+			{
+				count++;
+				sum += new_start;
+			}
+		}
+		return (count);
+	}
 }
 
