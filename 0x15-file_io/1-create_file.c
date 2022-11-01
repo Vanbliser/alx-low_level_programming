579314dda2ac657b1c9c6315a95c932645379082
Vanbliser <ayogublossomisrael@gmail.com>
2022-11-01 21:09:20 +0000

    Add 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,8 +16,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	int fd;
+	size_t i;
+	ssize_t wr;
 
+	if (filename == NULL)
+		return (-1);
 
+	fd = open(filename, O_RDWR | O_CREAT | O_APPEND, 0600);
+	if (fd == -1)
+		return (fd);
 
+	for (i = 0; *(text_content + i) != '\0'; ++i)
+		;
+
+	*(text_content + i) = EOF;
+
+	wr = write(fd, text_content, i);
+	if (wr == -1)
+		return (wr);
+	close(fd);
+
+	return (1);
 }
 
