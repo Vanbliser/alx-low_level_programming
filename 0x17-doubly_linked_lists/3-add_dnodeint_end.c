c7402c859807440938e3e38ceeae5d287bbe3259
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-05-20 03:52:06 +0000

    Fix betty error

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,7 +13,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 	dlistint_t *current = *head;
-	
+
 	if (head == NULL)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
