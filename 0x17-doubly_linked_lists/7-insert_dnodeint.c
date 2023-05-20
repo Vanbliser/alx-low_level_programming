dc2dd3314540140c9f8820eb711123f992084a2f
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-05-20 04:58:24 +0000

    Edit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,26 +13,34 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = *h;
-	dlistint_t *new;
-	unsigned int i = -1;
+	dlistint_t *new = NULL;
+	dlistint_t *head = *h, *prev = *h;
+	unsigned int i;
 
-	if (h == NULL)
+	if (h == NULL || (head == NULL && idx > 0))
 		return (NULL);
-	while (current)
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	head = head->next;
+	for (i = 1; i < idx; i++)
 	{
-		++i;
-		if (idx == i)
-		{
-			new = malloc(sizeof(dlistint_t));
-			new->n = n;
-			current->prev->next = new;
-			new->prev = current->prev;
-			new->next = current;
-			current->prev = new;
-			return (new);
-		}
-		current = current->next;
+		if (prev->next == NULL)
+			return (NULL);
+		head = head->next;
+		prev = prev->next;
 	}
-	return (NULL);
+	if (head == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = head;
+	new->prev = prev;
+	prev->next = new;
+	if (head != NULL)
+		head->prev = new;
+	return (new);
 }
