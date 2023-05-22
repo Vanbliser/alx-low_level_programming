2bf249d7464b5b465f945b7d7fbab58306aee438
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-05-22 22:05:39 +0000

    Update libdynamic.so, main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN
 #define MAIN
 
-/* PREPROCESSOR DIRECTIVE */
-#include <stdlib.h> 
+#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -24,12 +23,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-/* HELPER FUNCTIONS */
-int char_to_int(char i);
-unsigned int is_present(char *str, char a);
-unsigned int is_complete(char *str, char *st);
-unsigned int is_present_in_string(char *str, char a);
-unsigned int is_equal(char *str, char *strr);
 
 #endif /*MAIN*/
