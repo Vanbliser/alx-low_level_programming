b886ca3d4d48822a0e3ceb64defa3390772a638a
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-09-13 20:44:10 +0000

    task 0

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGO_H
+#define SEARCH_ALGO_H
+
+
+#include <stdio.h>
+
+
+/* FUNCTION PROTOTYPES */
+
+int linear_search(int *array, size_t size, int value);
+
+
+
+#endif /* SEARCH_ALGO_H */
